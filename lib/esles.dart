e747704738338b1d0df5da67fd5f1a865af4d6b5
sedanerdem <sedanerdem@hotmail.com>
2019-05-02 21:44:54 +0300

    *esles degistirildi

--- lib/esles.dart
+++ lib/esles.dart
@@ -4,6 +4,7 @@ import 'ApiDatabase.dart';
 import 'mesajlasma.dart';
 import 'KULDATA.dart';
 import 'utils.dart';
+import 'settings.dart';
 
 class Esles extends StatefulWidget {
   @override
@@ -39,7 +40,11 @@ class EslesmeEkrani extends State<Esles> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
-        onPressed: () {},
+        onPressed: () {
+           Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Settings()));
+        },
         padding: EdgeInsets.all(12),
         color: Colors.deepPurpleAccent,
         child: Row(
@@ -99,7 +104,7 @@ class EslesmeEkrani extends State<Esles> {
 
   Widget eslesButton() {
     return SizedBox(
-      width: screenAwareSize(290, context),
+      width: screenAwareSize(300, context),
       height: 100,
       child: RaisedButton(
         shape: RoundedRectangleBorder(
@@ -141,35 +146,32 @@ class EslesmeEkrani extends State<Esles> {
       ),
     );
   }
-  Widget title(){
+
+  Widget title() {
     return Padding(
-      padding: const EdgeInsets.only(left:40.0),
+      padding: const EdgeInsets.only(left: 40.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Text(
-                    'Random',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontSize: 50,
-                        color: Colors.white),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Text(
-                    'Message',
-                    style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 70,
-                        color: Colors.white),
-                  ),
-                ),
-                
-                
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              'Random',
+              style: TextStyle(
+                  fontFamily: 'Montserrat', fontSize: 50, color: Colors.white),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              'Message',
+              style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 70,
+                  color: Colors.white),
+            ),
+          ),
         ],
       ),
     );
@@ -203,31 +205,29 @@ class EslesmeEkrani extends State<Esles> {
                 height: screenAwareSize(200, context),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    title()
-                  ],
+                  children: <Widget>[title()],
                 ),
               ),
-              SizedBox(height: screenAwareSize(40, context),),
+              SizedBox(
+                height: screenAwareSize(40, context),
+              ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                
                 children: <Widget>[
                   eslesButton(),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  profile(),
                   SizedBox(
-                    width: 10,
+                    height: 20,
                   ),
-                  settings()
-                ],
-              ),
-              
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      profile(),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      settings()
+                    ],
+                  ),
                 ],
               )
             ],
